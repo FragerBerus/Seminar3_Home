2030a732b138b08fc2158c9f9231436835259dd0
Berus Frager <gitmailru@yandex.ru>
2022-08-11 22:39:52 +0300

    Коммит файлов папки проекта Example021

---
+++ Example021/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21:
+// Напишите программу, которая принимает на
+// вход координаты двух точек и находит расстояние между
+// ними в 3D пространстве.
+Console.Write("Введите координату X первой точки A: ");
+double AX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y первой точки A: ");
+double AY = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Z первой точки A: ");
+double AZ = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату X второй точки B: ");
+double BX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y второй точки B: ");
+double BY = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Z второй точки B: ");
+double BZ = Convert.ToInt32(Console.ReadLine());
+double DistanceX = AX - BX;
+double DistanceY = AY - BY;
+double DistanceZ = AZ - BZ;
+double DistanceSqrt = Math.Sqrt((DistanceX * DistanceX) + (DistanceY * DistanceY) + (DistanceZ * DistanceZ));
+Console.WriteLine("Расстояние между точками A и B " + DistanceSqrt);
